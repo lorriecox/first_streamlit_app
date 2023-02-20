036dbc5229c34c7a377a0c582c21fc3a9ea1f181
lorriecox <33575917+lorriecox@users.noreply.github.com>
2023-02-20 10:53:42 -0700

    Update streamlit_app.py
    updated case of streamlit error

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ try:
 #prompt for fruit choice and save to variable
 	fruit_choice = streamlit.text_input('What fruit would you like information about?')
 	if not fruit_choice:
-		Streamlit.error("Please select a fruit to get information.")
+		streamlit.error("Please select a fruit to get information.")
 	else:
 		fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 		# store normalized version of response in variable
